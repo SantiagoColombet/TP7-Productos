db06ae861637a590276ac451a5cd13311847816a
santi <santi.colombet@gmail.com>
2025-06-29 21:21:42 -0300

    dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,28 +2,52 @@ import Home from '../pages/Home.jsx';
 import ProductoDetalle from '../pages/ProductoDetalle';
 import Productos from '../pages/Productos';
 import QuienesSomos from '../pages/QuienesSomos';
-import { Routes, Route, Link,  } from 'react-router-dom'
-import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './NavBar.css'
 
 function NavBar() {
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products/categories')
+      .then(response => setCategorias(response.data))
+      .catch(error => console.error("Error al obtener las categorías:", error));
+  }, []);
+
   return (
-    <>    
-    <nav className="navbar">
+    <>
+      <nav className="navbar">
+        <p>Fake Store API</p>
         <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/productodetalle">Producto Detalle</Link></li>
-        <li><Link to="/productos">Productos</Link></li>
-        <li><Link to="/quienessomos">Quienes Somos</Link></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/quienessomos">Quienes Somos</Link></li>
+          <li className="dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Productos
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <Link className="dropdown-item" to="/productos/all">Ver todos</Link>
+              </li>
+              {categorias.map((categoria) => (
+                <li key={categoria}>
+                  <Link className="dropdown-item" to={`/productos/${categoria}`}>{categoria}</Link>
+                </li>
+              ))}
+            </ul>
+          </li>
         </ul>
-    </nav>
-    <Routes>
+      </nav>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/productodetalle" element={<ProductoDetalle />} />
-        <Route path="/productos" element={<Productos />} />
+        <Route path="/productodetalle/:idProducto" element={<ProductoDetalle />} />
+        <Route path="/productos/:idCategoria" element={<Productos />} />
         <Route path="/quienessomos" element={<QuienesSomos />} />
-    </Routes>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import NavBar from '../components/NavBar.jsx';
 import Footer from '../components/Footer.jsx';
 
