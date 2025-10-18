dd302de17b127b2d70f29bb4a1e22952a0b761bc
santi <santi.colombet@gmail.com>
2025-10-18 20:47:25 -0300

    error useCart

--- src/context/useCart.ts
+++ src/context/useCart.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CartContext } from "./CartContext"; 
+import { CartContext } from "./cartContext";
 import type { CartContextType } from "../types";
 
 const useCart = (): CartContextType => {
